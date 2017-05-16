232e1654206f18c8170b421ae3f46a8542d4d93e
Damodaran <damodaran@inapp.com>
2017-05-16 23:35:43 +0530

    Styling and Search bar features added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyC6ssfuZOCO3S8oETXjbthe2duDAfR8mWY';
+import API_KEY from './config';
+//const API_KEY = 'AIzaSyC6ssfuZOCO3S8oETXjbthe2duDAfR8mWY';
 
 class App extends Component {
 
@@ -16,17 +17,23 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards');
+		
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
 			});
 		});
 	}
+
 	render() {
 		return (
 	  	<div>
-			<SearchBar />
+			<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
 			<VideoDetail video={this.state.selectedVideo} />
 			<VideoList 
 				onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
